7d8da13adf84fc50326673826fcb7daed1ce6651
ohflying <119745767@qq.com>
2018-04-23 10:37:35 +0800

    export ThemeProvider

--- index.js
+++ index.js
@@ -7,9 +7,10 @@
 
 import Theme from './src/Theme';
 import connectStyle from './src/connectStyle';
-
+import ThemeProvider from './src/ThemeProvider';
 export {
-    connectStyle
+    connectStyle,
+    ThemeProvider
 };
 
 export default Theme;
